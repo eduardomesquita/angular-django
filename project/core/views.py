5a026bab5a326a499573a6933ee0ed63b0476cdc
Eduardo Mesquita <eduardomesquita09@gmail.com>
2018-01-05 15:54:25 -0200

    adicionado app

--- project/core/views.py
+++ project/core/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'cart.html', {})
+
+
+def adicionar_item(request):
+    itens_pedido = request.session.get('itens_pedido', [])
+
+    item = json.loads(request.body)['item']
+    itens_pedido.append(item)
+
+    request.session['itens_pedido'] = itens_pedido
+    return HttpResponse(json.dumps(itens_pedido))
+
+
+def excluir_item(request, index):
+    itens_pedido = request.session.get('itens_pedido', [])
+
+    del itens_pedido[int(index)]
+
+    request.session['itens_pedido'] = itens_pedido
+    return HttpResponse(json.dumps(itens_pedido))
